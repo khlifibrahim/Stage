9c0896af0c36f57bcfa0dbdd9d0944bc85b2e23e
KHLIFI BRAHIM <me.brahimkhlifi@gmail.com>
2025-02-03 18:56:07 +0100

    turn https

--- Frontend/src/Api/axios.jsx
+++ Frontend/src/Api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Instance = axios.create({
-    baseURL: 'http://khlifibrahim.me:3000/api'
+    baseURL: 'https://khlifibrahim.me:3000/api'
 })
 
 export default Instance;
