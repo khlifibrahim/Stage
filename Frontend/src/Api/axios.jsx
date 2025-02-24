7ca27f41d48c9358b053836ee6e74cc2d74760b1
KHLIFI BRAHIM <me.brahimkhlifi@gmail.com>
2025-02-24 15:47:34 +0000

    change axios

--- Frontend/src/Api/axios.jsx
+++ Frontend/src/Api/axios.jsx
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 const Instance = axios.create({
-    // baseURL: 'https://khlifibrahim.me:3000/api',
-    baseURL: 'http://localhost:5000/api'
+    // online: 'https://backend-zeta-liart-77.vercel.app/',
+    // local: 'http://localhost:5000/api'
+
+    baseURL : process.env.NODE_ENV === 'development' 
+    ? 'https://backend-zeta-liart-77.vercel.app/'
+    : 'http://localhost:5000/api'
 })
 
 export default Instance;
