8df4360d6bcc20dc79a0f7765f6d08e2f2122735
KHLIFI BRAHIM <me.brahimkhlifi@gmail.com>
2025-02-25 11:23:24 +0000

    db'

--- Backend/database/connectDB.js
+++ Backend/database/connectDB.js
@@ -1,20 +1,6 @@
 import fs from "fs";
 import path from "path";
-// import mongoose from "mongoose";
 import mysql from 'mysql2/promise';
-
-
-// export const connectDB = async () => {
-//     try {
-//         console.log("mongo_uri(line 5): " , process.env.MONGO_URI)
-//         const con = await mongoose.connect(process.env.MONGO_URI)
-//         console.log(`Mongo connected: ${con.connection.host}`)
-//     }catch (e) {
-//         console.log(`Error connect => `,e.message)
-//         process.exit(1)
-//     }
-// }
-
 
 
 export const connectSQL = async () => {
@@ -32,13 +18,6 @@ export const connectSQL = async () => {
         });
     
         console.log('Connected to the MySQL database.', process.env.DB_NAME , ' ', process.env.DB_WB);
-    
-        
-        // const [rows, fields] = await connection.execute('SELECT * FROM profile');
-        // console.log('Data fetched successfully:', rows);
-    
-        
-        // await connection.end();
 
         return connection;
       } catch (error) {
