16597ba93189902058662fe66e427321613f645c
KHLIFI BRAHIM <me.brahimkhlifi@gmail.com>
2025-03-16 07:34:20 +0000

    Update controls

--- Frontend/src/Pages/Mission/ListMissions.jsx
+++ Frontend/src/Pages/Mission/ListMissions.jsx
@@ -224,7 +224,7 @@ function ListMissions({role, user}) {
 
 
           {orderMissions.length > 0 ? handleFilterChange.slice(start, end).map((mission, i) => (
-            <div key={i} className="table-rows flex !flex-nowrap items-center justify-evenly max-lg:flex-wrap-reverse max-lg:justify-start max-lg:items-start flex-wrap py-3 my-2 border border-[#E4E4E4] rounded-[10px] cursor-pointer transition-colors hover:bg-[#F9F9F9] hover:!border-[#E4E4E4]">
+            <div key={i} className="table-rows flex lg:flex-nowrap items-center justify-evenly max-lg:flex-wrap-reverse max-lg:justify-start max-lg:items-start py-3 my-2 border border-[#E4E4E4] rounded-[10px] cursor-pointer transition-colors hover:bg-[#F9F9F9] hover:!border-[#E4E4E4]">
               <div onClick={() => handleDetails(mission.mission_id)} className="table-base-row px-3 w-full max-lg:basis-1/2"><p className="text-[#727272] rounded bg-transparent border-none max-lg:font-bold max-lg:text-lg">{`${mission.cadre_nom} ${mission.cadre_prenom}` || 'Mission name'}</p></div>
               <div onClick={() => handleDetails(mission.mission_id)} className="table-base-row px-3 w-full max-lg:basis-1/2 "><p className="text-[#727272] rounded bg-transparent border-none ">{mission.Object_type || 'Aucun'}</p></div>
               <div onClick={() => handleDetails(mission.mission_id)} className="table-base-row px-3 w-full max-lg:hidden "><p className="text-[#727272] rounded bg-transparent border-none">{mission.Destination || 'Oujda angade'}</p></div>
