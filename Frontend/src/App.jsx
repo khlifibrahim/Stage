bc986589686c12fa0187dcac7aeaf365f6555b2e
KHLIFI BRAHIM <me.brahimkhlifi@gmail.com>
2025-03-20 21:27:26 +0000

    update routing refresh

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './Redux/Store';
 import AppRoutes from './Routes/App.routes';
--- Frontend/src/Routes/App.routes.jsx
+++ Frontend/src/Routes/App.routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
 import { ProtectedRoute } from './Protected.route'
 
